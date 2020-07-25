73399f0a5794556fa2a533957231d0a865917f54
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-07-25 12:51:03 +0100

    setted up NavBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { Router } from "@reach/router";
-import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
