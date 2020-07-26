54e4ab1af3cf32b5328348e9738d05eb84f00583
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-07-26 12:18:05 +0100

    made a navigation bar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import "./navbar.css";
+import Toggle from "./Toggle";
 
-function NavBar() {
+const NavBar = (props) => {
   return (
-    <div>
-      <h1>This is the NavBar</h1>
-    </div>
+    <header className="toolbar">
+      <nav className="toolbar_navigation">
+        <div className="toggle-toolbar-button">
+          <Toggle click={props.drawerClickHandler} />
+        </div>
+        <div className="toolbar_logo">
+          <a href="/">Home</a>
+        </div>
+        <div className="spacer" />
+        <div className="toolbar-nav-items">
+          <ul>
+            <li>
+              <a href="/contacts">contacts</a>
+            </li>
+            <li>
+              <a href="/aboutme">About Me</a>
+            </li>
+            <li>
+              <a href="/experience">Projects</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
-}
+};
 
 export default NavBar;
