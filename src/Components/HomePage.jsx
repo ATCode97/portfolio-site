b4d3567f94371aa973b8f8c1a01355d0d450fd3f
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-07-31 17:47:18 +0100

    added icon to homepage

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { Icon, Grid } from "semantic-ui-react";
 
 const HomePage = () => {
   return (
     <div>
       <br></br>
       <br></br>
-      <h1>Landing Page</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Alistair Tsang
+      </h1>
+
+      <Grid>
+        <Grid.Column textAlign="center">
+          <Icon name="github icon" size="massive" />
+          <Icon name="linkedin icon" size="massive" />
+        </Grid.Column>
+      </Grid>
+
+      <br></br>
     </div>
   );
 };
