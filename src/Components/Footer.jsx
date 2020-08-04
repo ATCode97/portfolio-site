fe0b0144bf3952e7462f1362f6daa6cff42a07be
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-08-04 16:39:03 +0100

    added footer element

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Footer = () => {
+  return (
+    <Container>
+      <h5>Built with react</h5>
+    </Container>
+  );
+};
+
+export default Footer;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { Container, Divider } from "semantic-ui-react";
+import rbimg from "../Img/MaterialUI.png";
 
 const Projects = () => {
   return (
     <div>
       <br></br>
       <br></br>
-      <h1>Project and Cv</h1>
+      <h1
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Projects
+      </h1>
+      <Container textAlign="justified">
+        <Divider />
+        <img class="ui small centered image" src={rbimg} alt="project 1" />
+        <p>something about the backend project</p>
+      </Container>
     </div>
   );
 };
